deba383f2122285faf47faf0fe911ded13c336a7
Alex Feng <alex.feng.23@dartmouth.edu>
2020-04-16 04:12:29 -0700

    set up site

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let timer = 0;
+
+setInterval(() => {
+  $('#main').html(`You've been on this page for ${timer} seconds.`);
+  timer++;
+}, 1000);
